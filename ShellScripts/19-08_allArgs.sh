1a7ef7d8c3e044b93c1001b6f4417661b36f0a7c
Aditya Thakur <aditya1999thakur@gmail.com>
2019-08-19 16:00:16 +0530

    topic: basic prompts, echo, args,if, elif, else, file perms, switch case

---
+++ ShellScripts/19-08_allArgs.sh
@@ -0,0 +1,2 @@
+echo $@
+#Displaying all the arguments as separate strings here, also correct: $*
---
+++ ShellScripts/19-08_nameInput.sh
@@ -0,0 +1,4 @@
+read -p "enter your name: " first last
+echo "First Name: $first"
+echo "Last Name: $last"
+#the last variable gets the rest of the input line.
